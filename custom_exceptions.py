ae2c866a7baa280e7c0f2b7ac94dd63e61899abe
bavaaram <itsjustabavaar@gmail.com>
2023-05-27 01:56:08 +0330

    Add custom exceptions to custom_exceptions.py file.

---
+++ custom_exceptions.py
@@ -0,0 +1,28 @@
+class ShortPasswordError(Exception):
+    """
+    I use this Error when Short Password has been Entered.
+    """
+
+
+class PasswordError(Exception):
+    """
+    I use this Error when Wrong Password has been Entered.
+    """
+
+
+class UserError(Exception):
+    """
+    I use this Error When Wrong Username has been Entered.
+    """
+
+
+class RepUserError(Exception):
+    """
+    I use this Error When Repetitious Username has been Entered.
+    """
+
+
+class TwoPasswordError(Exception):
+    """
+    I use this Error When two New Passwords are not Match.
+    """
