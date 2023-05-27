2f97d1604905da1ddb798c146b0282eacbdfbe66
bavaaram <itsjustabavaar@gmail.com>
2023-05-27 21:46:37 +0330

    Implementation of Human Abstract class and declare some required sign up, sign in, edit and password change for it, then inherites User class from it and apply some edit on User class, Use super() function for calling Human abstract class __init__ magic method, edit some sections in User subclass.

--- human.py
+++ human.py
@@ -1,5 +1,7 @@
 #! /usr/bin/python3
 
+from abc import ABC, abstractmethod
+import datetime
 import uuid
 import hashlib
 import json
@@ -12,7 +14,176 @@ from custom_exceptions import (
 )
 
 
-class Human:
+class Human(ABC):
+    """
+    This is an Abstract class for Users and/
+    Admin
+    """
+    def __init__(self, fname: str, lname: str,
+                 username: str, password: str,
+                 birth_date: str, phone_number: str = None,
+                 user_id: str = None):
+        self.username, self.password = username, password
+        if user_id is None:
+            self.user_id = Human.uuid_gen()
+        else:
+            self.user_id = user_id
+        self.phone_number = phone_number
+        self.birth_date = birth_date
+
+    @staticmethod
+    def hashing(password: str):
+        """
+        This method is for hashing passwords
+        """
+        hashed_pass = (hashlib.sha256(password.encode("utf-8")).hexdigest())
+        return hashed_pass
+
+    @abstractmethod
+    def json_create(self):
+        """
+        This method creates a json file when there/
+        is no json file as a database to start the program.
+        """
+        pass
+
+    @staticmethod
+    def json_save(dictionary: dict):
+        """
+        This static method save or dump our class dictionary into/
+        database.json JSON fileeverytime use this method,/
+        the JSON file emptied and rewrite the class dictionary into it
+        """
+        pass
+
+    @abstractmethod
+    def json_import(self):
+        """
+        This class method import whole content of database.json file into/
+        our class dictionary with json.load() method
+        """
+        pass
+
+    @abstractmethod
+    def get_obj(self, username: str, password: str):
+        """
+        everytime we need to create an object in our class,/
+        and take data from our imported dictionary from/
+        database.json, such as login a user in out panel,/
+        we use this method. we give it the/username and/
+        password of the user and it will return an object/
+        for us
+        """
+        pass
+
+    def __str__(self):
+        """
+        This is a __str__ magic method for/
+        returning user Information as a string
+        """
+        return f"\nUser Information:\n\tUsername: {self.username}\n\tPhone Number: {self.phone_number}\n\tUser ID: {self.user_id}"
+
+    @abstractmethod
+    def sign_in_validation(self, user_name: str, passwd: str):
+        """
+        This method is for sign in validation/
+        and give username and password/
+        if given username not in usernames list/
+        rasing an Error that says Username not found
+        and if entered username and password is/
+        match to self.username and self.password
+        printing sign in complete,/
+        else if entered password is not match/
+        print wrong password error.
+        """
+        pass
+
+    @abstractmethod
+    def signup(self, user_name: str, passwd: str, ph_numb: str = None):
+        """
+        This function is for Signing up users.
+        first user must enter username, then enter password
+        and finally enter phone number
+        """
+        pass
+
+    @staticmethod
+    def username_check(user_name: str):
+        """
+        This static method actually for checking repetitious usernames
+        """
+        if user_name in User.all_usernames:
+            return False
+        return True
+
+    @abstractmethod
+    def edit_user(self, usr_name: str = None, ph_numb: str = None):
+        """
+        This method is used for username and/
+        phone number editing
+        if given username or phone number in not None/
+        , assigning given username and phone number/
+        to this instance Attributes
+        """
+        pass
+
+    @abstractmethod
+    def passwd_change(self, old_pass: str, new_pass: str, rep_new_pass: str):
+        """
+        This function is for password changing.
+        if entered old password in not match to original password/
+        or new password and Repeat it not match together/
+        raise an error.
+        """
+
+    @property
+    @abstractmethod
+    def username(self):
+        """
+        Getter for Username
+        """
+        pass
+
+    @username.setter
+    @abstractmethod
+    def username(self, user_value):
+        if not Human.username_check(user_value):
+            raise RepUserError("Username is already taken! ")
+        self._username = user_value
+
+    @staticmethod
+    def password_check(passwd: str) -> bool:
+        """
+        This function actually check the password and if its length smaller
+        than 4, an ValueError raised with the too short massage
+        """
+        if len(passwd) < 4:
+            return False
+        return True
+
+    @property
+    @abstractmethod
+    def password(self):
+        """
+        Getter for password
+        """
+        pass
+
+    @password.setter
+    @abstractmethod
+    def password(self, passwd_value):
+        pass
+
+    @staticmethod
+    def uuid_gen():
+        """
+        This function generate a universal unique identifier with uuid5
+        and use MD5 Hash algorithm
+        """
+        return str(uuid.uuid4())
+
+
+class User(Human):
     """
     This class is use for modeling users and some functionality/
     like username, password, a unique identifier and phone number./
@@ -23,33 +194,31 @@ class Human:
      it assuming to None
     """
     @staticmethod
-    def hashing(passwd: str) -> str:
+    def hashing(password: str) -> str:
         """
         this is a static method that generates hash from a password
         and unique salt for each user
         """
-        hashed_pass = (hashlib.sha256(passwd.encode("utf-8")).hexdigest())
+        hashed_pass = (hashlib.sha256(password.encode("utf-8")).hexdigest())
         return hashed_pass
 
     all_usernames = []
 
     def __init__(
-            self, username: str, password: str,
+            self, fname: str, lname: str,
+            username: str, password: str,
+            birth_date: str,join_date: str,
             phone_number: str = None,
             user_id: str = None
             ):
         """
         The __init__ method for assigning attributes
         """
-        self.username, self.password = username, password
-        if user_id is None:
-            self.user_id = Human.uuid_gen()
-        else:
-            self.user_id = user_id
-        self.phone_number = phone_number
-        if self.username not in Human.dictionary:
-            Human.dictionary.update({self.username: self.__dict__})
-            Human.json_save(Human.dictionary)
+        super().__init__(fname, lname, username, password,
+                         birth_date, phone_number, user_id)
+        if self.username not in User.dictionary:
+            User.dictionary.update({self.username: self.__dict__})
+            User.json_save(User.dictionary)
 
     @classmethod
     def json_create(cls):
@@ -108,7 +277,7 @@ class Human:
         return f"\nUser Information:\n\tUsername: {self.username}\n\tPhone Number: {self.phone_number}\n\tUser ID: {self.user_id}"
 
     @classmethod
-    def sign_in_validation(cls, user_name: str, passwd: str):
+    def sign_in_validation(cls, user_name: str, password: str):
         """
         This method is for sign in validation/
         and give username and password/
@@ -122,10 +291,10 @@ class Human:
         """
         if user_name not in cls.dictionary:
             raise UserError("Username not found! ")
-        new_key = Human.hashing(passwd)
+        new_key = Human.hashing(password)
         if cls.dictionary[user_name]["_User__password"] != new_key:
             raise PasswordError("Wrong Password!")
-        usr_obj = cls.get_obj(user_name, passwd)
+        usr_obj = cls.get_obj(user_name, password)
         return usr_obj
 
     dictionary = {}
@@ -145,7 +314,7 @@ class Human:
         """
         This static method actually for checking repetitious usernames
         """
-        if user_name in Human.all_usernames:
+        if user_name in User.all_usernames:
             return False
         return True
 
@@ -157,17 +326,17 @@ class Human:
         , assigning given username and phone number/
         to this instance Attributes
         """
-        if usr_name in Human.dictionary:
+        if usr_name in User.dictionary:
             raise RepUserError("Username already Taken! ")
         if usr_name != "":
-            del Human.dictionary[self.username]
+            del User.dictionary[self.username]
             self.username = usr_name
-            Human.dictionary.update({self.username: self.__dict__})
-            Human.json_save(Human.dictionary)
+            User.dictionary.update({self.username: self.__dict__})
+            User.json_save(User.dictionary)
         if ph_numb != "":
             self.phone_number = ph_numb
-            Human.dictionary[self.username]["phone_number"] = ph_numb
-            Human.json_save(Human.dictionary)
+            User.dictionary[self.username]["phone_number"] = ph_numb
+            User.json_save(User.dictionary)
 
     def passwd_change(self, old_pass: str, new_pass: str, rep_new_pass: str):
         """
@@ -182,8 +351,8 @@ class Human:
         if new_pass != rep_new_pass:
             raise TwoPasswordError("Unmatched new passwords")
         self.password = new_pass
-        Human.dictionary[self.username]["_User__password"] = self.password
-        Human.json_save(Human.dictionary)
+        User.dictionary[self.username]["_User__password"] = self.password
+        User.json_save(User.dictionary)
 
     @property
     def username(self):
