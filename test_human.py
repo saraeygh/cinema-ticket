7a1ac6eda08b24c5f8e5829dbe739db3da4c08e4
bavaaram <itsjustabavaar@gmail.com>
2023-05-28 13:44:11 +0330

    Add Three Test class for testing every functionality exists in human.py

---
+++ test_human.py
@@ -0,0 +1,28 @@
+#! /usr/bin/python3
+
+import unittest
+from human import Human, User, Admin
+
+
+class TestHuman(unittest.TestCase):
+    """
+    This test class is for testing Human Abstract class/
+    methods and functionality
+    """
+    pass
+
+
+class TestUser(unittest.TestCase):
+    """
+    This test class is for testing User class/
+    methods and functionality
+    """
+    pass
+
+
+class TestAdmin(unittest.TestCase):
+    """
+    This test class is for testing Admin class/
+    methods and functionality
+    """
+    pass
