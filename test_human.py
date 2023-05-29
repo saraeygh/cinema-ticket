ad3f6139d3e69f6338d7c6e7aee045bf072db55c
bavaaram <itsjustabavaar@gmail.com>
2023-05-29 13:17:55 +0330

    Add more test method to TestUser class.

--- test_human.py
+++ test_human.py
@@ -93,18 +93,57 @@ class TestUser(unittest.TestCase):
         cls.filepath = "./test_database/test.json"
         os.makedirs("./test_database")
         Human.json_create(TestHuman.filepath)
+    
+    def setUp(self):
+        self.user1 = User("Matin", "Ghane", "bavaar", "12345",
+                          "1999/11/20", "09197951537")
+        self.user2 = User("Ali", "Alikhani", "aliii",
+                          "qwerty", "1997/10/03", "09121111111")
 
     def test_get_obj(self):
-        user1 = User("Matin", "Ghane", "bavaar", "12345",
-                     "1999/11/20", "09197951537")
         with self.assertRaises(UserError):
             User.get_obj("Ali", "qwerty")
-        User.all_usernames.remove(user1.username)
+        User.all_usernames.remove(self.user1.username)
+        User.all_usernames.remove(self.user2.username)
         user2 = User.get_obj("bavaar", "12345")
-        self.assertEqual(user1.__str__(), user2.__str__())
+        user3 = User.get_obj("aliii", "qwerty")
+        test_dict = {user2.username: user2.__dict__,
+                     user3.username: user3.__dict__}
+        self.assertEqual(User.dictionary, test_dict)
 
     def test_sign_in_validation(self):
-        pass
+        with self.assertRaises(UserError):
+            User.sign_in_validation("Ali", "123456")
+        with self.assertRaises(PasswordError):
+            User.sign_in_validation("bavaar", "matinghane")
+        User.all_usernames.remove(self.user1.username)
+        User.all_usernames.remove(self.user2.username)
+        user2 = User.sign_in_validation("bavaar", "12345")
+        user3 = User.sign_in_validation("aliii", "qwerty")
+        test_dict = {user2.username: user2.__dict__,
+                     user3.username: user3.__dict__}
+        self.assertEqual(User.dictionary, test_dict)
+
+    def test_signup(self):
+        with self.assertRaises(RepUserError):
+            User("Ali", "Alikhani", "bavaar",
+                 "12345", "1999/10/18", "09365181897")
+        with self.assertRaises(ShortPasswordError):
+            User("mamad", "hosseini", "mamadi",
+                 "mam", "1997/03/25", "09122222222")
+
+    def test_edit_user(self):
+        with self.assertRaises(RepUserError):
+            self.user2.edit_user("bavaar")
+
+        self.user1.edit_user("matin_ghane", "09365181897")
+
+
+
+    def tearDown(self):
+        User.all_usernames.clear()
+        User.dictionary.clear()
+        del self.user1
 
     @classmethod
     def tearDownClass(cls):
