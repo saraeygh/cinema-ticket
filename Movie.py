e5ea50efb927d961b26b26c143540acaa490f35d
Mojtaba-anbahoun <mojtaba.anbahoun@gmail.com>
2023-05-30 08:33:34 +0330

    edit class Ticket

--- Movie.py
+++ Movie.py
@@ -1,65 +1,94 @@
+import datetime
 import json
+import os
+from zoneinfo import available_timezones
 
 class Film:
     """
     
     """
-    def __init__(self, name: str, genre: str, age_rating: str, scene_date: int, capacity: int):
+    def __init__(self, name: str, genre: str, age_rating: str, tickets: dict = {}):
         self.name = name
         self.genre = genre
         self.age_rating = age_rating
-        self.scene_date = scene_date
-        self.capacity = capacity
-        self.films.update({self.name: self.__dict__})
-        self.save_films_to_json()
+        self.tickets = tickets
+        Film.films.update({self.name: self.__dict__})
+        
     films = {}
 
-    def load_films_from_json(self):
+    @classmethod
+    def load_films_from_json(cls):
         #try:
         with open("database.json", 'r') as file:
-            Film.films = json.load(file)
+            return json.load(file)
         #except FileNotFoundError:
         #    self.films = {}
 
-    @staticmethod
-    def save_films_to_json():
+    @classmethod
+    def save_films_to_json(cls, dictionary):
         with open("database.json", 'w+') as file:
-            json.dump(Film.films, file, indent=4)
+            json.dump(dictionary, file, indent=4)
 
-    def add_film(self, name: str, genre: str, age_rating: str, scene_date: int, capacity: int):
-        film = Film(name, genre, age_rating, scene_date, capacity)
+    @classmethod
+    def add_film(cls, name: str, genre: str, age_rating: str, scene_date, showtime, capacity):
+        film = cls(name, genre, age_rating)
+        ticket = Ticket(name, scene_date, showtime, capacity)
+
      
     @classmethod
     def get_object(cls, name):
+        for i, j in Film.films.items():
+            if i == name:
+                return cls(j["name"],
+                            j["genre"],
+                            j["age_rating"],
+                            j["scene_date"],
+                            j["scene_time"],
+                            j["capacity"])
+
+    @classmethod   
+    def remove_film(cls, name: str):
         for film in Film.films:
-            if name in Film.films:
-                return cls()
-        
-    def remove_film(self, name: str):
-        for film in Film.films:
-            if film['name'] == self.name:
-                del Film.films[self.name]
-                self.save_films_to_json()
+            if film == name:
+                del Film.films[name]
+                cls.save_films_to_json(Film.films)
                 break
 
+
 class Ticket(Film):
-    def __init__(self, name, genre, age_rating, capacity, showtime):
-        super().__init__(name, genre, age_rating)
-        self.capacity = capacity
+    ticket_dict = {}
+    def __init__(self, name, scene_date, showtime, capacity):
+        #super().__init__(name, genre, age_rating)
+        self.name = name
+        self.scene_date = scene_date
         self.showtime = showtime
         self.available_seats = capacity
+        Ticket.ticket_dict.update({f"{self.scene_date} _ {self.showtime}": self.__dict__})
+        for film in Film.films:
+            if film == self.name:
+                Film.films[film]["tickets"] = Ticket.ticket_dict
+        Film.save_films_to_json(Film.films)
+
+    @staticmethod
+    def add_ticket(name, scene_date, showtime, capacity):
+        return Ticket(name, scene_date, showtime, capacity)
 
     def sell_ticket(self, quantity):
         if quantity <= self.available_seats:
             self.available_seats -= quantity
+            Ticket.ticket_dict["capacity"] = self.available_seats
+            Film.films[self.name]["tickets"]["capacity"] = self.available_seats
+            Film.save_films_to_json(Film.films)
             print(f"{quantity} ticket(s) sold successfully.")
         else:
             print("Insufficient available seats.")
 
-#film_manager = Film("name", "a" , "b", "c", "v")
-#film_manager.save_films_to_json()
-
 while True:
+    if os.path.isfile("database.json"):
+        Film.films = Film.load_films_from_json()
+    else:
+        Film.save_films_to_json({})
+
     print("1. Add Film")
     print("2. Remove Film")
     print("3. Exit")
@@ -70,21 +99,20 @@ while True:
         name = input("Enter film name: ")
         genre = input("Enter film genre (Comedy / Action / Family / Romance): ")
         age_rating = int(input("Enter film age rating: "))
-        scene_date = {
-            'year' : int(input("Enter the year of release of the movie: ")),
-            'month' : int(input("Enter the month of release of the movie: ")),
-            'day' : int(input("Enter the day of release of the movie: ")),
-            'hour' : int(input("Enter the hour of release of the movie: "))
-        }
+        year, month, day = input("Enter the scene_date of release of the movie: ").split("-")
+        scene_date = datetime.date(int(year), int(month), int(day)).isoformat()
+        hour , minute = input("Enter hour and minute: ").split(":")
+        scene_time = datetime.time(hour=int(hour), minute=int(minute)).isoformat(timespec='minutes')
+        
         capacity = int(input("Enter the capacity of the movie theater: "))
-        film = Film(name, genre, age_rating, scene_date, capacity)
-        Film.save_films_to_json()
+
+        Film.add_film(name, genre, age_rating, scene_date, scene_time, capacity)
+       
         print("Film added successfully!")
         print()
     elif choice == 2:
         name = input("Enter film name to remove: ")
-        Film.remove_film()
-        #film_manager.remove_film(name)
+        Film.remove_film(name)
         print("Film removed successfully!")
         print()
     elif choice == 3:
@@ -92,93 +120,5 @@ while True:
     else:
         print("Invalid choice. Please try again.")
         print()
-
-        
-import json
-
-class Film:
-    def __init__(self, file_path):
-        self.file_path = file_path
-        self.films = []
-
-    def load_films_from_json(self):
-        try:
-            with open(self.file_path, 'r') as file:
-                self.films = json.load(file)
-        except FileNotFoundError:
-            self.films = []
-
-    def save_films_to_json(self):
-        with open(self.file_path, 'w') as file:
-            json.dump(self.films, file, indent=4)
-
-    def add_film(self, name, genre, age_rating):
-        film = {
-            'name': name,
-            'genre': genre,
-            'age_rating': age_rating
-        }
-        self.films.append(film)
-        self.save_films_to_json()
-
-    def remove_film(self, name):
-        for film in self.films:
-            if film['name'] == name:
-                self.films.remove(film)
-                self.save_films_to_json()
-                break
-
-class Ticket(Film):
-    def __init__(self, name, genre, age_rating, capacity, showtime):
-        super().__init__(name, genre, age_rating)
-        self.capacity = capacity
-        self.showtime = showtime
-        self.available_seats = capacity
-
-    def charge_ticket(self, quantity):
-        if quantity <= self.available_seats:
-            self.available_seats -= quantity
-            print(f"{quantity} ticket(s) charged successfully.")
-        else:
-            print("Insufficient available seats.")
-
-    def sell_ticket(self, quantity):
-        if quantity <= self.available_seats:
-            self.available_seats -= quantity
-            print(f"{quantity} ticket(s) sold successfully.")
-        else:
-            print("Insufficient available seats.")
-
-   
-
-film_manager = Film('films.json')
-film_manager.load_films_from_json()
-
-while True:
-    print("1. Add Film")
-    print("2. Remove Film")
-    print("3. Exit")
-
-    choice = int(input("Enter your choice: "))
-
-    if choice == 1:
-        name = input("Enter film name: ")
-        genre = input("Enter film genre (Comedy / Action / Family / Romance): ")
-        age_rating = int(input("Enter film age rating: "))
-        film_manager.add_film(name, genre, age_rating)
-        print("Film added successfully!")
-        print()
-    elif choice == 2:
-        name = input("Enter film name to remove: ")
-        film_manager.remove_film(name)
-        print("Film removed successfully!")
-        print()
-    elif choice == 3:
-        break
-    else:
-        print("Invalid choice. Please try again.")
-        print()
-
-
 
         
