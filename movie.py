1cf5db1613f317ac8781d4dd4c160e06838034ea
bavaaram <itsjustabavaar@gmail.com>
2023-05-30 15:46:38 +0330

    fix some minor issues

--- movie.py
+++ movie.py
@@ -34,7 +34,8 @@ class Film:
                 tickets data in the dictionary\
                 into a json file called database/films.json
         """
-        with open("./database/films.json", mode="w+", encoding="utf-8") as file:
+        with open("./database/films.json",
+                  mode="w+", encoding="utf-8") as file:
             json.dump(dictionary, file, indent=4)
 
     @classmethod
