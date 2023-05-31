fcd7eded23c79b6ce4bb0fef1731f81c64812941
bavaaram <itsjustabavaar@gmail.com>
2023-05-31 06:50:04 +0330

    Major Editng on plan.py file

--- plan.py
+++ plan.py
@@ -1,86 +1,93 @@
-#!/usr/bin/python3
-
+"""
+This module contains Plan, Silver and Glod classes/
+for modeling various types of user plan
+"""
+from abc import ABC, abstractmethod
 from human import User
-from datetime import datetime, timedelta
+import datetime
 from bank_accounts import BankAccount
 
-class Plan():
-   def __init__(self, username):
-       self.username = username
-
+class Plan(ABC):
+    def __init__(self, username, record_date):
+        self.username = username
+        self.record_date = record_date
 
 # Define user-active mode function
-   def active(self, username):
-       pass
-
+    @abstractmethod
+    def active(self, username):
+        pass
 
 # Define user-deactive mode function
-   def deactive(self, username):
-       pass
-
+    @abstractmethod
+    def deactive(self, username):
+        pass
 
 # Definition of discount part function
-   def use_plan(self, username):
-       pass
+    @abstractmethod
+    def use_plan(self, username):
+        pass
+
+    def __str__(self):
+        pass
 
 
 class Silver(Plan):
-   
-   PRICE = 100_000
-   DISCOUNT = 0.2
 
-   def __init__ (self):
-       super().__init__()
-       
-   def active(self, username):
-       User.change_plan(self, username, "Silver")
-       credit = 3
-       # Save credit in user json.
-       BankAccount.withdraw(self, Silver.PRICE)
+    PRICE = 100_000
+    DISCOUNT = 0.2
 
-   def deactive(self, username):
-       User.change_plan(self, username, "Bronze")
+    def __init__ (self, username, record_date, credit):
+        super().__init__(username, record_date)
 
-   @classmethod
-   def use_plan(self, username, cost, user_credit):
-      if user_credit == 0:
-          print("you have no credit.")
-          Silver.deactive(self, username)
-      else:
-          user_credit -= 1
-          return (cost * (1 - Silver.DISCOUNT))
-   User.charge_wallet += PRICE * DISCOUNT  
-   def __str__(self):
-       # get user_credit from json
-       return f"Remanied count: {self.user_credit}"
-   
+    def active(self, username):
+        User.change_plan(self, username, "Silver")
+        credit = 3
+        # Save credit in user json.
+        BankAccount.withdraw(self, Silver.PRICE)
+
+    def deactive(self, username):
+        User.change_plan(self, username, "Bronze")
+
+    @classmethod
+    def use_plan(self, username, cost, user_credit):
+        if user_credit == 0:
+            print("you have no credit.")
+            Silver.deactive(self, username)
+        else:
+            user_credit -= 1
+            return cost * (1 - Silver.DISCOUNT)
+    User.charge_wallet += PRICE * DISCOUNT
+
+    def __str__(self):
+        # get user_credit from json
+        return f"Remanied count: {self.user_credit}"
+
 
 class Gold(Plan):
 
-   PRICE = 500_000
-   DISCOUNT = 0.5
+    PRICE = 500_000
+    DISCOUNT = 0.5
 
-   def __init__(self, username):
-       super().__init__(username)
+    def __init__(self, username, record_date, credit):
+        super().__init__(username)
+ 
+    def active(self, username):
+        User.change_plan(self, username, "Gold")
+        start_date = datetime.now()
+        expire_date = start_date + timedelta(days=30)
+        # Save to json
+ 
+    def deactive(self, username):
+        User.change_plan(self, username, "Bronze")
+    
+    def use_plan(self, username, cost):
+        # exipre_date = Load user info from json
+        if datetime.now() >  expire_date:
+            print("Your gold plan expired.")
+            Gold.deactive()
+        else:
+            return (cost * (1 - Gold.DISCOUNT)), "Energy drink"
+            BankAccount.withdraw(self, Gold.PRICE)
 
-   def active(self, username):
-      User.change_plan(self, username, "Gold")
-      start_date = datetime.now()
-      expire_date = start_date + timedelta(days=30)
-      # Save to json
-
-   def deactive(self, username):
-       User.change_plan(self, username, "Bronze")
-   
-   def use_plan(self, username, cost):
-      # exipre_date = Load user info from json
-      if datetime.now() >  expire_date:
-          print("Your gold plan expired.")
-          Gold.deactive()
-      else:
-          return (cost * (1 - Gold.DISCOUNT)), "Energy drink"
-          BankAccount.withdraw(self, Gold.PRICE)
-      
-
-   def __str__(self):
-         return f"remanied expire_date: {self.user_credit}"
+    def __str__(self):
+        return f"remanied expire_date: {self.user_credit}"
