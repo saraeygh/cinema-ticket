b48cd00cfb9de71a3266595f78d575a5d8285573
bavaaram <itsjustabavaar@gmail.com>
2023-05-31 05:47:22 +0330

    Sign up admins via CLI feature Added, move admins.json loading or creating operation to first of our main.py file and implementation Exit the program in Admin sign up CLI with sys.exit()

--- main.py
+++ main.py
@@ -1,20 +1,39 @@
 #! /usr/bin/python3
 
+import sys
 import datetime
+import argparse
 from getpass import getpass
 import os
-from movie import Film, Ticket
+from movie import Film
 from custom_exceptions import (
-        UserError,
-        RepUserError,
-        PasswordError,
-        TwoPasswordError,
-        ShortPasswordError,
-        FilmError,
-        NoCapacityError
-        )
+    UserError,
+    RepUserError,
+    PasswordError,
+    TwoPasswordError,
+    ShortPasswordError,
+    FilmError,
+)
 
 from human import Human, User, Admin
+
+if os.path.exists("./database/admins.json"):
+    Admin.dictionary = Human.json_import("./database/admins.json")
+else:
+    Human.json_save("./database/admins.json", {})
+
+parser = argparse.ArgumentParser(description="Take Username and Password from CLI and Create an Admin with those Information.")
+parser.add_argument("-u", "--username", type=str, help="Username of New Admin")
+parser.add_argument("-p", "--password", type=str, help="Password of New Admin")
+args = parser.parse_args()
+try:
+    Admin.signup(args.username, args.password)
+except RepUserError:
+    print("Username Already Taken! \n")
+    sys.exit("Exiting the Admin Creating Interface...")
+else:
+    print("Admin Created! \n")
+    sys.exit("Exiting the Admin Creating Interface...")
 
 if not os.path.isdir("./database"):
     os.mkdir("./database")
@@ -41,9 +60,9 @@ while 1:
                 birth_date = input("Enter Birth date (YYYY/MM/DD): ")
                 phone_number = input("Enter Phone number(Optional): ")
                 try:
-                    User.signup(fname, lname, username,
-                                password, birth_date,
-                                phone_number)
+                    User.signup(
+                        fname, lname, username, password, birth_date, phone_number
+                    )
                 except RepUserError:
                     print("\nUsername Already Taken! ")
                 except ShortPasswordError:
@@ -67,41 +86,52 @@ while 1:
 
                 while 1:
                     print("\n************ - User Dashboard - ************\n")
-                    stat = input("Stat (1(Show User Information) - 2(Edit) - 3(Show Tickets menu) - 4(Reserve Ticket) - 5(Cahrge Wallet) - 6(Back to Main Menu)):   ")
+                    stat = input(
+                        "Stat (1(Show User Information) - 2(Edit) - 3(Show Tickets menu) - 4(Reserve Ticket) - 5(Cahrge Wallet) - 6(Back to Main Menu)):   "
+                    )
                     if stat == "1":
                         print(user_object)
                         print("List of Bank Accounts:")
-                    
+
                     elif stat == "2":
                         while 1:
                             print("\n***** ^ Edit User mode ^ *****\n")
-                            stat = input("Stat 1(Edit profile) - 2(Password Change) - 3(Show Current Plan) - 4(Change Plan) - 5(Edit Bank Accounts) - 6(Back to Dashboard)   ")
-    
+                            stat = input(
+                                "Stat 1(Edit profile) - 2(Password Change) - 3(Show Current Plan) - 4(Change Plan) - 5(Edit Bank Accounts) - 6(Back to Dashboard)   "
+                            )
+
                             if stat == "1":
                                 print("\n***** ^ Edit Profile mode ^ *****\n")
-                                print("For Abort to editing any item, just leave it and press Enter.")
+                                print(
+                                    "For Abort to editing any item, just leave it and press Enter."
+                                )
                                 new_fname = input("Enter New First Name: ")
                                 new_lname = input("Enter New Last Name: ")
-                                new_b_date = input("Enter New Birth Date (YYYY-MM-DD): ")
+                                new_b_date = input(
+                                    "Enter New Birth Date (YYYY-MM-DD): "
+                                )
                                 new_usr = input("Enter New Username: ")
                                 new_ph_numb = input("New Phone Number: ")
                                 try:
-                                    user_object.edit_user(new_fname, new_lname,
-                                                          new_usr, new_ph_numb,
-                                                          new_b_date)
+                                    user_object.edit_user(
+                                        new_fname,
+                                        new_lname,
+                                        new_usr,
+                                        new_ph_numb,
+                                        new_b_date,
+                                    )
                                 except RepUserError:
                                     print("\nUsername already Taken! ")
                                 else:
                                     print("\nUser Information has been Updated! ")
-    
+
                             elif stat == "2":
                                 print("\n******** ^ Password Change ^ ********\n")
                                 old_p = getpass("Old Password: ")
                                 new_p = getpass("New Password: ")
                                 re_new_p = getpass("New Password again: ")
                                 try:
-                                    user_object.password_change(old_p,
-                                                                new_p, re_new_p)
+                                    user_object.password_change(old_p, new_p, re_new_p)
                                 except PasswordError:
                                     print("\nWrong Original Password! ")
                                 except TwoPasswordError:
@@ -110,23 +140,25 @@ while 1:
                                     print("Two Short New Password! ")
                                 else:
                                     print("\nYour Password has been changed! ")
-    
+
                             elif stat == "3":
                                 print("***** ^ Show Current Plan ^ *****")
-    
+
                             elif stat == "4":
                                 print("***** ^ Change Plan ^ *****")
-    
+
                             elif stat == "5":
                                 print("***** ^ Edit Bank Accounts ^ *****")
-                                print("Stat 1(Add a Bank Account -\
-                                        2(Edit Bank Accounts))")
+                                print(
+                                    "Stat 1(Add a Bank Account -\
+                                        2(Edit Bank Accounts))"
+                                )
                                 if stat == "1":
                                     print("***** ^ Add Bank Account ^ *****")
-    
+
                                 elif stat == "2":
                                     print("***** ^ Edit Bank Accounts ^ *****")
-    
+
                     elif stat == "3":
                         print("***** ^ Ticket Menu ^ *****")
 
@@ -154,11 +186,6 @@ while 1:
                 continue
 
     elif stat == "2":
-        if os.path.exists("./database/admins.json"):
-            Admin.dictionary = Human.json_import("./database/admins.json")
-        else:
-            Human.json_save("./database/admins.json", {})
-
         while 1:
             print("\n******** - admin management panel - ********\n")
             print()
@@ -185,20 +212,24 @@ while 1:
 
                 while 1:
                     print("\n************ - Admin Dashboard - ************\n")
-                    stat = input("Stat (1(Add film) - 2(Remove film) - 3(Add Ticket) - 4(Show Your Information) - 5(Edit Username) - 6(Password Change) - 7(Back to Main Menu)):   ")
+                    stat = input(
+                        "Stat (1(Add film) - 2(Remove film) - 3(Add Ticket) - 4(Show Your Information) - 5(Edit Username) - 6(Password Change) - 7(Back to Main Menu)):   "
+                    )
                     if stat == "1":
                         print("\n************** ^ Add film ^ **************\n")
                         film_name = input("Enter Film name: ")
-                        film_genre = input("Enter Film Genre (Comedy/Action/Family/Romance): ")
+                        film_genre = input(
+                            "Enter Film Genre (Comedy/Action/Family/Romance): "
+                        )
                         age_rate = input("Enter film Age Rating: ")
                         Admin.add_film(film_name, film_genre, age_rate)
-                       print("\nFilm added successfully! \n")
+                        print("\nFilm added successfully! \n")
 
                     elif stat == "2":
                         print("********** ^ Remove Film ^ **********")
                         film_name = input("Enter Film Name to Remove: ")
                         try:
-                            Film.remove_film(film_name)
+                            Admin.remove_film(film_name)
                         except FilmError:
                             print("Film with this Name not found for Remove! ")
                         else:
@@ -207,16 +238,23 @@ while 1:
                     elif stat == "3":
                         print("***** ^ Add Ticket ^ *****")
                         film_name = input("Enter Film Name")
-                        year, month, day = input("Enter scene date (YYYY-MM-DD): ").split("-")
-                        film_date = datetime.date(int(year), int(month), int(day)).isoformat()
+                        year, month, day = input(
+                            "Enter scene date (YYYY-MM-DD): "
+                        ).split("-")
+                        film_date = datetime.date(
+                            int(year), int(month), int(day)
+                        ).isoformat()
                         hour, minute = input("Enter Scene time (HH:MM): ")
-                        scene_time = datetime.time(hour=int(hour), minute=int(minute)).isoformat(timespec='minutes')
+                        scene_time = datetime.time(
+                            hour=int(hour), minute=int(minute)
+                        ).isoformat(timespec="minutes")
                         ticket_capacity = int(input("Enter the Scene Capacity: "))
                         try:
-                            Admin.add_show(film_name, film_date, scene_time, ticket_capacity)
+                            Admin.add_show(
+                                film_name, film_date, scene_time, ticket_capacity
+                            )
                         except Exception:
                             print("Invalid truncation, please Try again.")
- 
 
                     elif stat == "4":
                         print(admin_object)
@@ -238,7 +276,9 @@ while 1:
                         new_pass = getpass("Enter New Password: ")
                         rep_new_pass = getpass("Enter New Password again: ")
                         try:
-                            admin_object.password_change(old_pass, new_pass, rep_new_pass)
+                            admin_object.password_change(
+                                old_pass, new_pass, rep_new_pass
+                            )
                         except PasswordError:
                             print("\nWrong Original Password! ")
                         except TwoPasswordError:
